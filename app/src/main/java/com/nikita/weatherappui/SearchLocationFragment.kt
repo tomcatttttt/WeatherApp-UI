87aba08363a3e51f3599b6a437fb20291eb8217c
nikitakaretnikov <myipodtouch2014@gmail.com>
2024-11-20 12:33:03 +0200

    Added smooth animation for button text and forecast visibility toggling

---
+++ app/src/main/java/com/nikita/weatherappui/SearchLocationFragment.kt
@@ -0,0 +1,27 @@
+package com.nikita.weatherappui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nikita.weatherappui.databinding.FragmentSearchLocationBinding
+
+class SearchLocationFragment : Fragment() {
+
+    private var _binding: FragmentSearchLocationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchLocationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
